87110e70fe1e8bf3440343ccc8667fe91871b2c0
Douglas A. Seifert <doug@dseifert.net>
2017-04-21 13:48:17 -0700

    fix: support session update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :sessions, only: [:index, :show, :create, :destroy]
+  resources :sessions, only: [:index, :show, :create, :destroy, :update]
   root 'chords#show'
   resource :chords, only: [:show, :update]
 end
