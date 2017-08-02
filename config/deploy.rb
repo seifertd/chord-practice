0162ac46374f6d81a202b414ec76e151394e6624
Douglas A. Seifert <doug@dseifert.net>
2017-08-02 13:18:35 -0700

    chore: grab production database from shared folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :scm_verbose, true
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "db/production.sqlite3"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
