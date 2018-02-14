db82dfb1c4bdc1c6198c3835fc5213cf08c1fa4f
Douglas A. Seifert <doug@dseifert.net>
2018-02-14 15:58:12 -0800

    fix: implement barred frets

--- frontend/components/fret-board.vue
+++ frontend/components/fret-board.vue
@@ -81,22 +81,34 @@
         let stringPos = fretBoard.stringGap;
         let fretPos = fretBoard.fretGap;
         for (let i = 1; i < (fretBoard.numStrings - 1); ++i) {
-          drawing.line(stringPos + fretBoard.left, 0 + fretBoard.top, stringPos + fretBoard.left, fretBoard.height + fretBoard.top).stroke({width: 1});
-          drawing.line(0 + fretBoard.left, fretPos + fretBoard.top, fretBoard.width + fretBoard.left, fretPos + fretBoard.top).stroke({width: 1});
+          drawing.line(stringPos + fretBoard.left, 0 + fretBoard.top,
+            stringPos + fretBoard.left,
+            fretBoard.height + fretBoard.top).stroke({width: 1});
+          drawing.line(0 + fretBoard.left, fretPos + fretBoard.top,
+            fretBoard.width + fretBoard.left,
+            fretPos + fretBoard.top).stroke({width: 1});
           stringPos += fretBoard.stringGap;
           fretPos += fretBoard.fretGap;
         }
         if (fretBoard.chord) {
           let string = 6;
           let bar = null;
-          if (fretBoard.chord.notes.length > 6) {
-            bar = fretBoard.chord.notes[6];
+          if (fretBoard.chord.notes.length > fretBoard.numStrings) {
+            bar = fretBoard.chord.notes[fretBoard.numStrings];
+            let [_dummy, barFret, barStrings] = bar.split("-");
+            if (_dummy === 'B') {
+              let barY = fretBoard.top - fretBoard.noteRadius * 3.4 + fretBoard.fretGap * barFret;
+              let startString = fretBoard.numStrings - parseInt(barStrings[0]);
+              let endString = fretBoard.numStrings - parseInt(barStrings[1]);
+              let startX = fretBoard.left + fretBoard.stringGap * startString - fretBoard.noteRadius;
+              let endX = fretBoard.left + fretBoard.stringGap * endString + fretBoard.noteRadius;
+              let width = endX - startX;
+              drawing.rect(width,fretBoard.noteRadius*2).radius(10).x(startX).y(barY).stroke({width: 2}).fill("black");
+            }
           }
           for (let string = 6; string > 0; --string) {
             let stringIdx = fretBoard.numStrings - string;
             let fret = fretBoard.chord.notes[stringIdx];
-            stringPos = fretBoard.stringGap * stringIdx;
-            fretPos = fretBoard.fretGap * fret;
             if (fret >= 0) {
               let fill = fret == 0 ? "white" : "black";
               let circle = drawing.circle(fretBoard.noteRadius*2).x(fretBoard.left + fretBoard.stringGap * stringIdx - fretBoard.noteRadius).
@@ -104,7 +116,7 @@
                 stroke({width: 1}).fill(fill);
             } else {
               drawing.text("x").x(fretBoard.left * 1.2 + fretBoard.stringGap * stringIdx - fretBoard.noteRadius).
-                y(fretBoard.top + 2 + fretBoard.fretGap * fret).font({size: fretBoard.noteRadius * 4});
+                y(fretBoard.top + 2 + fretBoard.fretGap * fret - 1).font({size: fretBoard.noteRadius * 4});
             }
           }
         }
