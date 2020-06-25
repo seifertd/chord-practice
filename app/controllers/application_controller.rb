10461579dbf36f13ac75cb68d44f8b3fec45e88f
Douglas A. Seifert <doug@dseifert.net>
2020-06-24 23:40:09 -0700

    Rails upgrade; dep upgrades

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :current_player
   def current_player
-    player_id = cookies[:player_id] || session.id
+    player_id = cookies[:player_id] || session.id&.public_id
     @current_player ||= Player.find_or_create_by(uuid: player_id)
   end
 end
